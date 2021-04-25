6928d4ffc5db1262d9d0b56fc04adb9307de5a67
thecreata21 <yerezhepabdulla@gmail.com>
2021-04-25 19:42:04 +0600

    adding validation so that UserName and Password are Required fields to fill

--- API/DTOs/DTORegister.cs
+++ API/DTOs/DTORegister.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.DTOs
 {
     public class DTORegister
     {
+        [Required]
         public string UserName {get; set;}
+
+        
+        [Required]
         public string Password {get; set;}
     }
 }
