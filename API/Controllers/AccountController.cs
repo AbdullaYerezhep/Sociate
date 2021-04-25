87ddc801af455293ba4ce5f9295dad091d1a6157
thecreata21 <yerezhepabdulla@gmail.com>
2021-04-25 21:14:14 +0600

    DTO login + login method added

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -37,6 +37,25 @@ namespace API.Controllers
             return user;    
         }
 
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(DTOLogin dtoLogin)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == dtoLogin.UserName);
+
+            if(user == null) return Unauthorized("Invalid username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(dtoLogin.Password));
+
+            for(int i = 0; i < computedHash.Length; i++)
+            {
+                if(computedHash[i]!=user.PasswordHash[i]) return Unauthorized("Invalid password");
+            }
+
+            return user;
+        }
+
         private async Task<bool> UserExists(string username){
             return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
