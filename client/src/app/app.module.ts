506258f9059a7ffe88fb954d0995c766a2e46fe4
thecreata21 <yerezhepabdulla@gmail.com>
2021-04-28 23:57:40 +0600

    dropdown in navbar logout functionality added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +16,7 @@ import { NavComponent } from './nav/nav.component';
     NavComponent
   ],
   imports: [
+    BsDropdownModule.forRoot(), 
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
